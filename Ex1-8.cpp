7dba43fe4c87225279cabeba5010c0a798bf1552
LingNinghuang <138959332+LingNinghuang@users.noreply.github.com>
2023-07-10 13:58:16 +0800

    Create Ex1-8.cpp

---
+++ Ex1-8.cpp
@@ -0,0 +1,11 @@
+//constants
+#include <stdio.h>
+int main() {
+    const int minutesperhour = 60;
+    const float pi = 3.14;
+    const int BIRTHMONTH = 12;
+    printf("%d\n", minutesperhour);
+    printf("%f\n", pi);
+    printf("%d", BIRTHMONTH);
+    return 0;
+}
